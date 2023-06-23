b22edbe09e7aea37bc77d7e95fe5a5981ac2f4ff
cbecerra11 <136035946+cbecerra11@users.noreply.github.com>
2023-06-22 19:04:33 -0500

    hello-world

--- src/App.js
+++ src/App.js
@@ -6,8 +6,9 @@ function App() {
     <div className="App">
 Nathan_branch
       <h1>Hello world!</h1>
-      <h1>Hello world! This is Nathan from Indiana!</h1>
-
+      
+<h1>Hello world! This is Nathan from Indiana!</h1>
+<h1>Hello world! This is Christians Branch!</h1>
       <h1>Hello world! I'm Brett Bohlsen and this is another branch.</h1>
  main
     </div>
